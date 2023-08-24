1b594d45800f7494f9641587b0a31d283bf379e6
ddcas9 <ddcas9@users.noreply.github.com>
2023-08-24 07:45:00 -0400

    fixed image dynamic server-renders

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const nextConfig = {
   // This option tells Next.js to use the production build when exporting a static site.
   output: "export",
+  images: {
+    unoptimized: true,
+  }
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,177 @@
-import { useState } from "react"
-import Head from "next/head"
-import dynamic from "next/dynamic"
+// OLD CODE
 
-import styles from "../styles/Home.module.css"
-import Image from "next/image"
 
-const CodeSampleModal = dynamic(() => import("../components/CodeSampleModal"), {
-  ssr: false
-})
+// import { useState } from "react"
+// import Head from "next/head"
+// import dynamic from "next/dynamic"
 
-export default function Start({ countries }) {
-  const [results, setResults] = useState(countries)
-  const [isModalOpen, setIsModalOpen] = useState(false)
+// import styles from "../styles/Home.module.css"
+// import Image from "next/image"
+
+// const CodeSampleModal = dynamic(() => import("../components/CodeSampleModal"), {
+//   ssr: false
+// })
+
+// export default function Start({ countries }) {
+//   const [results, setResults] = useState(countries)
+//   const [isModalOpen, setIsModalOpen] = useState(false)
+
+//   return (
+//     <div>
+//       <Head>
+//         <title>Core Web Vitals</title>
+//         <meta name="description" content="Core web vitals walk through" />
+//         <link rel="icon" href="/favicon.ico" />
+//         <link
+//           href="https://fonts.googleapis.com/css2?family=Inter"
+//           rel="stylesheet"
+//         />
+//       </Head>
+
+//       <main className={styles.container}>
+//         <h1 className={styles.title}>
+//           Powered by <a href="https://nextjs.org">Next.js!</a>
+//         </h1>
+
+//         <div className={styles.heroImage}>
+//           <Image
+//             src="/large-image.jpg"
+//             alt="Large Image"
+//             width={3048}
+//             height={2024}
+//           />
+//         </div>
+
+//         <div>
+//           <h2 className={styles.secondaryHeading}>Population Lookup</h2>
+//           <input
+//             type="text"
+//             placeholder="Country search..."
+//             className={styles.input}
+//             onChange={async (e) => {
+//               const { value } = e.currentTarget
+//               // Dynamically load libraries
+//               const Fuse = (await import("fuse.js")).default
+//               const _ = (await import("lodash")).default
+
+//               const fuse = new Fuse(countries, {
+//                 keys: ["name"],
+//                 threshold: 0.3
+//               })
+
+//               const searchResult = fuse
+//                 .search(value)
+//                 .map((result) => result.item)
+
+//               const updatedResults = searchResult.length
+//                 ? searchResult
+//                 : countries
+//               setResults(updatedResults)
+
+//               // Fake analytics hit
+//               console.info({
+//                 searchedAt: _.now()
+//               })
+//             }}
+//           />
+
+//           <ul className={styles.countries}>
+//             {results.map((country) => (
+//               <li key={country.cca2} className={styles.country}>
+//                 <p>
+//                   {country.name} - {country.population.toLocaleString()}
+//                 </p>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+
+//         <div className={styles.codeSampleBlock}>
+//           <h2 className={styles.secondaryHeading}>Code Sample</h2>
+//           <p>Ever wondered how to write a function that prints Hello World?</p>
+//           <button onClick={() => setIsModalOpen(true)}>Show Me</button>
+//           {isModalOpen && (
+//             <CodeSampleModal
+//               isOpen={isModalOpen}
+//               closeModal={() => setIsModalOpen(false)}
+//             />
+//           )}
+//         </div>
+//       </main>
+
+//       <footer className={styles.footer}>
+//         <a
+//           href="https://vercel.com?utm_source=learn&&utm_campaign=core-web-vitals"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Powered by
+//           <span className={styles.logo}>
+//             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+//           </span>
+//         </a>
+//       </footer>
+//     </div>
+//   )
+// }
+
+// export async function getServerSideProps() {
+//   const response = await fetch("https://restcountries.com/v3.1/all")
+//   const countries = await response.json()
+
+//   return {
+//     props: {
+//       countries: countries.map((country) => ({
+//         name: country.name.common,
+//         cca2: country.cca2,
+//         population: country.population,
+//       })),
+//     }
+//   }
+// }
+
+
+// NEW USEEFFECT CODE (GPT 3.5)
+
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Fuse from "fuse.js";
+import _ from "lodash";
+
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+
+import CodeSampleModal from "../components/CodeSampleModal";
+
+export default function Start({ initialCountries }) {
+  const [results, setResults] = useState(initialCountries);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const searchHandler = async (value) => {
+      const fuse = new Fuse(initialCountries, {
+        keys: ["name"],
+        threshold: 0.3,
+      });
+
+      const searchResult = fuse.search(value).map((result) => result.item);
+      const updatedResults = searchResult.length ? searchResult : initialCountries;
+      setResults(updatedResults);
+
+      console.info({
+        searchedAt: _.now(),
+      });
+    };
+
+    const inputElement = document.querySelector(".search-input");
+    inputElement.addEventListener("input", (e) => {
+      searchHandler(e.target.value);
+    });
+
+    return () => {
+      inputElement.removeEventListener("input", searchHandler);
+    };
+  }, [initialCountries]);
 
   return (
     <div>
@@ -36,6 +196,7 @@ export default function Start({ countries }) {
             alt="Large Image"
             width={3048}
             height={2024}
+            priority // For image preloading for static gh-pages
           />
         </div>
 
@@ -44,34 +205,8 @@ export default function Start({ countries }) {
           <input
             type="text"
             placeholder="Country search..."
-            className={styles.input}
-            onChange={async (e) => {
-              const { value } = e.currentTarget
-              // Dynamically load libraries
-              const Fuse = (await import("fuse.js")).default
-              const _ = (await import("lodash")).default
-
-              const fuse = new Fuse(countries, {
-                keys: ["name"],
-                threshold: 0.3
-              })
-
-              const searchResult = fuse
-                .search(value)
-                .map((result) => result.item)
-
-              const updatedResults = searchResult.length
-                ? searchResult
-                : countries
-              setResults(updatedResults)
-
-              // Fake analytics hit
-              console.info({
-                searchedAt: _.now()
-              })
-            }}
+            className={styles.input + " search-input"}
           />
-
           <ul className={styles.countries}>
             {results.map((country) => (
               <li key={country.cca2} className={styles.country}>
@@ -109,20 +244,24 @@ export default function Start({ countries }) {
         </a>
       </footer>
     </div>
-  )
+  );
 }
 
-export async function getServerSideProps() {
-  const response = await fetch("https://restcountries.com/v3.1/all")
-  const countries = await response.json()
+export async function getStaticProps() {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const countries = await response.json();
+
+  const initialCountries = countries.map((country) => ({
+    name: country.name.common,
+    cca2: country.cca2,
+    population: country.population,
+  }));
 
   return {
     props: {
-      countries: countries.map((country) => ({
-        name: country.name.common,
-        cca2: country.cca2,
-        population: country.population,
-      })),
-    }
-  }
+      initialCountries,
+    },
+  };
 }
+
+
