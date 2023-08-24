11069288007de7bf9a07cfb91b632f621497618d
ddcas9 <ddcas9@users.noreply.github.com>
2023-08-24 07:15:20 -0400

    minimal bard config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 const nextConfig = {
-  // This is a custom configuration option.
-  myCustomOption: "foobar",
-
-  // This is the experimental images feature.
-  images: {
-    disableStaticImages: true,
-  },
-
   // This option tells Next.js to use the production build when exporting a static site.
-  output: {
-    export: true,
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
